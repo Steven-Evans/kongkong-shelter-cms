611656636f5e1ac72e7851ad4a8d68eeaf1913bb
Steven Evans <1st3v3n7@gmail.com>
2020-07-14 19:41:46 -0700

    Add lifecycle methods to rebuild frontend when cms data is changed

--- api/posting/models/posting.js
+++ api/posting/models/posting.js
@@ -1,8 +1,26 @@
 'use strict';
 
+const axios = require('axios');
+const frontendWebhookBuildURL = strapi.config.custom.frontendWebhookBuildURL;
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(entry) {
+      if (frontendWebhookBuildURL)
+        axios.post(frontendWebhookBuildURL, entry);
+    },
+    async afterUpdate(entry) {
+      if (frontendWebhookBuildURL)
+        axios.post(frontendWebhookBuildURL, entry);
+    },
+    async afterDelete(entry) {
+      if (frontendWebhookBuildURL)
+        axios.post(frontendWebhookBuildURL, entry);
+    },
+  }
+};
---
+++ config/env/production/custom.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+  frontendWebhookBuildURL: env('FRONTEND_WEBHOOK_BUILD_URL'),
+});
